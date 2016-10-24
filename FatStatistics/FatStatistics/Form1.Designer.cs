b7c22b0fa4da814ac0553d91cad79e1be18deae7
vargmurtter <krugerlive@mail.ru>
2016-10-25 01:44:37 +0300

    Add files via upload

---
+++ FatStatistics/FatStatistics/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace FatStatistics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tech = new System.Windows.Forms.RadioButton();
+            this.hum = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.weight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.age = new System.Windows.Forms.TextBox();
+            this.sex = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.user_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user_weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user_age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.user_sex = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.result = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tech
+            // 
+            this.tech.AutoSize = true;
+            this.tech.Location = new System.Drawing.Point(154, 13);
+            this.tech.Name = "tech";
+            this.tech.Size = new System.Drawing.Size(67, 17);
+            this.tech.TabIndex = 0;
+            this.tech.TabStop = true;
+            this.tech.Text = "Технарь";
+            this.tech.UseVisualStyleBackColor = true;
+            // 
+            // hum
+            // 
+            this.hum.AutoSize = true;
+            this.hum.Location = new System.Drawing.Point(154, 36);
+            this.hum.Name = "hum";
+            this.hum.Size = new System.Drawing.Size(91, 17);
+            this.hum.TabIndex = 1;
+            this.hum.TabStop = true;
+            this.hum.Text = "Гуманитарий";
+            this.hum.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label.Location = new System.Drawing.Point(6, 134);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 16);
+            this.label.TabIndex = 3;
+            // 
+            // weight
+            // 
+            this.weight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.weight.Location = new System.Drawing.Point(75, 13);
+            this.weight.Name = "weight";
+            this.weight.Size = new System.Drawing.Size(49, 23);
+            this.weight.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Вес: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(4, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Возраст:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Пол:";
+            // 
+            // age
+            // 
+            this.age.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.age.Location = new System.Drawing.Point(75, 52);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(49, 23);
+            this.age.TabIndex = 8;
+            // 
+            // sex
+            // 
+            this.sex.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.sex.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sex.FormattingEnabled = true;
+            this.sex.Items.AddRange(new object[] {
+            "М",
+            "Ж"});
+            this.sex.Location = new System.Drawing.Point(75, 88);
+            this.sex.Name = "sex";
+            this.sex.Size = new System.Drawing.Size(49, 24);
+            this.sex.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.user_type,
+            this.user_weight,
+            this.user_age,
+            this.user_sex});
+            this.dataGridView1.Location = new System.Drawing.Point(249, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(466, 400);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // user_type
+            // 
+            this.user_type.HeaderText = "User Type";
+            this.user_type.Name = "user_type";
+            // 
+            // user_weight
+            // 
+            this.user_weight.HeaderText = "User Weight";
+            this.user_weight.Name = "user_weight";
+            // 
+            // user_age
+            // 
+            this.user_age.HeaderText = "User Age";
+            this.user_age.Name = "user_age";
+            // 
+            // user_sex
+            // 
+            this.user_sex.HeaderText = "User Sex";
+            this.user_sex.Name = "user_sex";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(246, 442);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 17);
+            this.result.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(640, 442);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Обновить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 202);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(210, 211);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 491);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.sex);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.weight);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.hum);
+            this.Controls.Add(this.tech);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Fat Statistics";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton tech;
+        private System.Windows.Forms.RadioButton hum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox weight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox age;
+        private System.Windows.Forms.ComboBox sex;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user_type;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user_weight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user_age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn user_sex;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FatStatistics/FatStatistics/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace FatStatistics
+{
+    public partial class Form1 : Form
+    {
+
+        private string DBhost = "HOST";
+        private string DBuser = "USER";
+        private string DBpass = "PASS";
+        private string DB = "DATABASE";
+
+        int TechRes;
+        int HumRes;
+        int user;
+        string userType;
+        string userSex;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            if (tech.Checked) user = 1;
+            else if (hum.Checked) user = 0;
+
+            if (weight.Text != "" && age.Text != "" && sex.SelectedIndex.ToString() != "-1")
+            {
+
+                MySqlCommand command = new MySqlCommand(); ;
+                string connectionString, commandString;
+                connectionString = "Data source=" + DBhost + ";UserId=" + DBuser + ";Password=" + DBpass + ";database=" + DB + ";";
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                commandString = @"INSERT INTO `users` (`user_type`, `weight`, `age`, `sex`) 
+                                VALUES ('" + user + "', '" + weight.Text + "', '" + age.Text + "', '" + sex.SelectedIndex.ToString() + "');";
+                command.CommandText = commandString;
+                command.Connection = connection;
+                MySqlDataReader reader;
+
+                try
+                {
+                    command.Connection.Open();
+                    reader = command.ExecuteReader();
+                    reader.Close();
+
+                    if (Convert.ToInt32(user) == 1) userType = "технарь";
+                    else userType = "гуманитарий";
+
+                    if (Convert.ToInt32(sex.SelectedIndex) == 0) userSex = "М";
+                    else userSex = "Ж";
+
+                    int rowNumber = dataGridView1.Rows.Add();
+                    
+                    dataGridView1.Rows[rowNumber].Cells["user_type"].Value = userType;
+                    dataGridView1.Rows[rowNumber].Cells["user_weight"].Value = weight.Text;
+                    dataGridView1.Rows[rowNumber].Cells["user_age"].Value = age.Text;
+                    dataGridView1.Rows[rowNumber].Cells["user_sex"].Value = userSex;
+
+
+                    label.ForeColor = Color.Green;
+                    label.Text = "Новые данные успешно добавлены!";
+                    sex.SelectedIndex = -1;
+                    age.Text = "";
+                    weight.Text = "";
+
+                }
+                catch (MySqlException ex)
+                {
+                    label.ForeColor = Color.Red;
+                    label.Text = ex.ToString();
+                }
+                finally
+                {
+                    command.Connection.Close();
+                }
+
+            }
+            else
+            {
+                label.ForeColor = Color.Red;
+                label.Text = "Вы не заполнили одно из полей!";
+            }
+
+
+        }
+
+        private void getData()
+        {
+
+            MySqlCommand command = new MySqlCommand(); ;
+            string connectionString, commandString;
+            connectionString = "Data source=" + DBhost + ";UserId=" + DBuser + ";Password=" + DBpass + ";database=" + DB + ";";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            commandString = "SELECT * FROM `users`;";
+            command.CommandText = commandString;
+            command.Connection = connection;
+            MySqlDataReader reader;
+            try
+            {
+                command.Connection.Open();
+                reader = command.ExecuteReader();
+                TechRes = 0;
+                HumRes = 0;
+                while (reader.Read())
+                {
+
+                    if (Convert.ToInt32(reader["user_type"]) == 1) userType = "технарь";
+                    else userType = "гуманитарий";
+
+                    if (Convert.ToInt32(reader["sex"]) == 0) userSex = "М";
+                    else userSex = "Ж";
+
+                    int rowNumber = dataGridView1.Rows.Add();
+                    
+                    dataGridView1.Rows[rowNumber].Cells["user_type"].Value = userType;
+                    dataGridView1.Rows[rowNumber].Cells["user_weight"].Value = reader["weight"];
+                    dataGridView1.Rows[rowNumber].Cells["user_age"].Value = reader["age"];
+                    dataGridView1.Rows[rowNumber].Cells["user_sex"].Value = userSex;
+
+                    /*----------------*/
+
+                    
+
+                    if (Convert.ToInt32(reader["user_type"]) == 1) TechRes += Convert.ToInt32(reader["weight"]);
+                    else if (Convert.ToInt32(reader["user_type"]) == 0) HumRes += Convert.ToInt32(reader["weight"]);
+
+
+
+                }
+
+                if (TechRes < HumRes) result.Text = "Технари легче, чем гуманитарии!";
+                else if (TechRes > HumRes) result.Text = "Технари тяжелее, чем гуманитарии!";
+                else if (TechRes == HumRes) result.Text = "Технари и гуманитарии весят одинакого!";
+
+                reader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error: \r\n{0}", ex.ToString());
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
